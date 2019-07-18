c67ad53f2f96dd7a70a2597b91f36383a7d77374
jinfull <jinfull@gmail.com>
2019-07-17 21:02:27 -0700

    Link up backend localhost:3001 with served proxied data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import './App.css';
+// import React from 'react';
+// import './App.css';
 
-class App extends React.Component {
+// class App extends React.Component {
   
 
 
+//   render() {
+//     return (
+//       <div>hello</div>
+//     )
+//   }
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { articles: [] };
+  }
+  componentDidMount() {
+    fetch('http://localhost:3001/')
+      .then(response => response.json())
+      .then(data => console.log(data));
+      // .then(data => this.setState({ data }))
+      // .catch(error => alert(error.message));
+  }
   render() {
     return (
-      <div>hello</div>
-    )
+      <div className="App">
+        <h1>Users</h1>
+        {/* {this.state.users.map(user =>
+          <div key={user.id}>user: {user.name} Password: {user.password}</div>
+        )} */}
+      </div>
+    );
   }
 }
-
 export default App;
